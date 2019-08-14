3e83d509b09d7a1d28fb7510fa74565386328d53
Вячеслав <bvebars@gmail.com>
2019-08-14 18:22:56 +0700

    Полный рефакторинг, немного ООП, и сокращение кода.

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -1,5 +1,8 @@
-let inputs = document.getElementsByTagName('input');
-let phoned = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; //XXX-XXX-XXXX
+let formService = document.getElementById('form-service');
+let rateService = document.getElementById('rate-service');
+let formData = document.getElementById('form-data');
+let inputsFormData = formData.getElementsByTagName("input");
+let inputsFormService = formService.getElementsByTagName("input");
 let userData = new Map();
 
 class Form {
@@ -7,9 +10,11 @@ class Form {
         this.form = form;
     }
 
-    static gettingValueForm() {
-        userData.clear();
-        for (let input of form.form) {
+    gettingValueForm() {
+        // userData.clear();
+        this.number = 0;
+        let phoned = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; //XXX-XXX-XXXX
+        for (let input of this.form) {
             cleanInput(input);
             if (input.value !== "") {
 
@@ -17,30 +22,72 @@ class Form {
 
                     if (!input.value.match(phoned)) {
                         alert('Введите корректный номер');
+                        this.number--;
                         document.getElementById(input.name).classList.add('error');
+                    } else {
+                        userData.set(input.name, input.value);
+                        this.number++;
                     }
-                    userData.set(input.name, input.value);
+
                 }
                 userData.set(input.name, input.value);
+                this.number++;
             } else {
                 if (input.name !== 'phone2') {
                     document.getElementById(input.name).classList.add('error');
                 }
             }
         }
-        console.log(userData);
+
+        console.log("N: " + this.number);
     }
 
-    static toggleForm (){
+    went() {
+        console.log("This " + this.number);
+        return this.form.length <= this.number;
+    }
+
+    static showFormService() {
+        formData.style.display = "none";
+        formService.style.display = "block";
+        rateService.style.display = "none";
+    }
+
+    static showRateService() {
         formData.style.display = "none";
         formService.style.display = "none";
+        rateService.style.display = "block";
+    }
+
+    static showFormService() {
+        formData.style.display = "none";
+        formService.style.display = "block";
         rateService.style.display = "none";
     }
 }
 
-let form = new Form(inputs);
-Form.toggleForm();
 
+let form1 = new Form(inputsFormData);
+let form2 = new Form(inputsFormService);
+
+function goFormService() {
+    form1.gettingValueForm();
+    console.log(userData);
+
+    if (form1.went()) {
+        Form.showFormService();
+    }
+}
+
+function goRateService() {
+    form2.gettingValueForm();
+    console.log(userData);
+
+    if (form2.went()) {
+        Form.showRateService();
+        addFieldText();
+    }
+}
 
 //---- Удаление класса
 function cleanInput(id) {
@@ -51,216 +98,63 @@ function cleanInput(id) {
     });
 }
 
-let goButton = document.getElementById("go-button");
-// let hide = document.getElementById("hide");
-// let rateShow = document.getElementById("rateShow");
-let formData = document.getElementById('form-data');
-let formService = document.getElementById('form-service');
-let rateService = document.getElementById('rate-service');
-//
-// function toggleForm() {
-//     formData.style.display = "none";
-//     formService.style.display = "none";
-//     rateService.style.display = "none";
-// }
 
-// goButton.addEventListener("click", () => {
-//     formData.style.display = "none";
-//     formService.style.display = "none";
-//     rateService.style.display = "none";
-// });
+let nameData = document.getElementById('name-data');
+let cityData = document.getElementById('city-data');
+let addressData = document.getElementById('address-data');
+let phoneData = document.getElementById('phone-data');
+let phoneData2 = document.getElementById('phone-data-2');
+let appealData = document.getElementById('appeal-data');
+let commentData = document.getElementById('comment-data');
+let tariff = document.getElementById('tariff');
 
-// show.addEventListener("click", () => {
-//     formData.style.display = "none";
-//     formService.style.display = "block";
-//     rateService.style.display = "none";
-// });
-//
-// rateShow.addEventListener("click", () => {
-//     formData.style.display = "none";
-//     formService.style.display = "none";
-//     rateService.style.display = "block";
-// });
+function addFieldText() {
 
-// user.gettingValueUser();
+    nameData.textContent = userData.get('name');
+    cityData.textContent = userData.get('city');
+    addressData.textContent = userData.get('address');
+    phoneData.textContent = userData.get('phone');
+    phoneData2.textContent = userData.get('phone2');
+    appealData.textContent = userData.get('appeal');
+    tariff.textContent = userData.get('rate');
+    commentData.textContent = userData.get('comment');
+}
 
-//-------------------------------------------------------------------------------------------------
-//
-// let array = new Map();
-// // let city = document.getElementById('city');
-// // let name = document.getElementById('name');
-// // let address = document.getElementById('address');
-// // let phone = document.getElementById('phone');
-// // let error = document.getElementById('error');
-// // let further = document.getElementById('further');
-// // let rate = document.getElementById('rate');
-// // let appeal = document.getElementById('appeal');
-// // let comment = document.getElementById('comment');
-//
-//
-//
-//
-//
-// //Обработка события onChange
-// function getId(id) {
-//     id.onchange = (() => {
-//         if (id.classList.contains('error')) {
-//             id.classList.remove("error");
-//         }
-//     });
-// }
-//
-// //Оработка вызова кнопки формы 1 "Далее"
-// function valid() {
-//     let phoned = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; //XXX-XXX-XXXX
-//
-//     if (newUser.city.value === "") {
-//         error.textContent = 'Введите город';
-//         document.getElementById('city').classList.add('error');
-//         getId(city);
-//         return;
-//     } else if (name.value === "") {
-//         error.textContent = 'Введите ФИО';
-//         document.getElementById('name').classList.add('error');
-//         getId(name);
-//         return;
-//     } else if (address.value === "") {
-//         error.textContent = 'Введите адресс';
-//         document.getElementById('address').classList.add('error');
-//         getId(address);
-//         return;
-//     }
-//
-//     if (!phone.value.match(phoned)) {
-//         if (phone.value === "") {
-//             error.textContent = 'Введите телефон';
-//         } else {
-//             error.textContent = 'Проверьте заполненность данных телефона';
-//         }
-//         document.getElementById('phone').classList.add('error');
-//         getId(phone);
-//         return;
-//     }
-//
-//     if (appeal.value === "") {
-//         error.textContent = 'Введите обращение';
-//         document.getElementById('appeal').classList.add('error');
-//         getId(appeal);
-//         return;
-//     }
-//
-//     error.textContent = 'Данные отправлены';
-//     formData.style.display = "none";
-//     formService.style.display = "block";
-//
-//     array.set('city', city.value);
-//     array.set('name', name.value);
-//     array.set('address', address.value);
-//     array.set('phone', phone.value);
-//     array.set('appeal', appeal.value);
-//
-//     if (comment.value !== '') {
-//         array.set('comment', comment.value);
-//     }
-//
-//     console.log(array);
-//
-// }
-//
-// function checkPhone() {
-//     let phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; //XXX-XXX-XXXX
-//     if (phone.value.match(phoneno)) {
-//         error.textContent = 'Проверьте заполненность данных телефона';
-//         return;
-//     }
-// }
-//
-// let formData = document.getElementById('form-data');
-// let formService = document.getElementById('form-service');
-// let rateService = document.getElementById('rate-service');
-// let show = document.getElementById("show");
-// let hide = document.getElementById("hide");
-// let rateShow = document.getElementById("rateShow");
-// // let hide = document.getElementById("hideContent")
-//
-// hide.addEventListener("click", () => {
-//     formData.style.display = "block";
-//     formService.style.display = "none";
-//     rateService.style.display = "none";
-// });
-//
-// show.addEventListener("click", () => {
-//     formData.style.display = "none";
-//     formService.style.display = "block";
-//     rateService.style.display = "none";
-// });
-//
-// rateShow.addEventListener("click", () => {
-//     formData.style.display = "none";
-//     formService.style.display = "none";
-//     rateService.style.display = "block";
-// });
-//
-// //Объявление переменных для вывода информацию в блоке service
-//
-// let nameData = document.getElementById('name-data');
-// let cityData = document.getElementById('city-data');
-// let addressData = document.getElementById('address-data');
-// let phoneData = document.getElementById('phone-data');
-// let appealData = document.getElementById('appeal-data');
-// let commentData = document.getElementById('comment-data');
-// let tariff = document.getElementById('tariff');
-//
-//
-// //Третья форма
-// let emergencyClosure = document.getElementById('emergency-closure');
-//
-// function sendForth() {
-//     array.set('rate', rate.value);
-//
-//     formData.style.display = "none";
-//     formService.style.display = "none";
-//     rateService.style.display = "block";
-//
-//     addFieldText();
-// }
-//
-// function sendBack() {
-//     formData.style.display = "block";
-//     formService.style.display = "none";
-//     rateService.style.display = "none";
-// }
-//
-// let failure = document.getElementById('failure');
-// let commentFailure = document.getElementById('comment-failure');
-//
-// function sendComplete() {
-//     array.set('failure', failure.value);
-//     array.set('commentFailure', commentFailure.value);
-//     //Тут должна быть реализация отправки данных
-//
-//     array.clear();
-//     fieldCleaning();
-//
-//     formData.style.display = "none";
-//     formService.style.display = "none";
-//     rateService.style.display = "none";
-//     emergencyClosure.style.display = "block"
-// }
-//
-// function addFieldText() {
-//     nameData.textContent = array.get('name');
-//     cityData.textContent = array.get('city');
-//     addressData.textContent = array.get('address');
-//     phoneData.textContent = array.get('phone');
-//     appealData.textContent = array.get('appeal');
-//     tariff.textContent = array.get('rate');
-//     commentData.textContent = array.get('comment');
-// }
-//
-// function fieldCleaning() {
-//     let inputs = document.getElementsByTagName('input');
-//     for (let input of inputs) {
-//         input.value = '';
-//     }
-// }
+let show = document.getElementById("show");
+
+show.addEventListener("click", () => {
+    formData.style.display = "none";
+    formService.style.display = "block";
+    rateService.style.display = "none";
+});
+
+function sendBack() {
+    formData.style.display = "block";
+    formService.style.display = "none";
+    rateService.style.display = "none";
+}
+
+let failure = document.getElementById('failure');
+let commentFailure = document.getElementById('comment-failure');
+let emergencyClosure = document.getElementById('emergency-closure');
+
+function sendComplete() {
+    userData.set('failure', failure.value);
+    userData.set('commentFailure', commentFailure.value);
+    //Тут должна быть реализация отправки данных
+
+    userData.clear();
+    fieldCleaning();
+
+    formData.style.display = "none";
+    formService.style.display = "none";
+    rateService.style.display = "none";
+    emergencyClosure.style.display = "block"
+}
+
+function fieldCleaning() {
+    let inputs = document.getElementsByTagName('input');
+    for (let input of inputs) {
+        input.value = '';
+    }
+}
